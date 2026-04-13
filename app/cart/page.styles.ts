be25bbbaa31953368b1f9a6d1d77138b0aa0f8d6
juliemeta <juliemonefeldt_404@hotmail.com>
2026-04-13 11:44:17 +0200

    refactor: extracted the quantity select buttons' styles to separate file

--- app/cart/page.styles.ts
+++ app/cart/page.styles.ts
@@ -1,5 +1,5 @@
 import { styled } from "@mui/material/styles";
-import { Box, Grid } from "@mui/material";
+import { Box, Button, Grid } from "@mui/material";
 
 export const CartPageContainer = styled(Grid)({
   alignItems: "start",
@@ -15,4 +15,20 @@ export const CartContent = styled(Box)(({ theme }) => ({
   marginBottom: theme.spacing(3),
   borderBottom: "1px solid #eee",
   paddingBottom: theme.spacing(2),
+}));
+
+export const SelectQuantityWrapper = styled(Box)(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  gap: theme.spacing(1),
+  marginTop: theme.spacing(1),
+}));
+
+export const SelectQuantityButton = styled(Button)(({ theme }) => ({
+  width: theme.spacing(4),
+  height: theme.spacing(4),
+  borderRadius: 4,
+  border: "1px solid #ccc",
+  background: theme.palette.background.elevated,
+  cursor: "pointer",
 }));
--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -4,7 +4,13 @@ import CartSummary from "@/components/cart/CartSummary";
 import { useCartStore } from "@/lib/store/cartStore";
 import Image from "next/image";
 import Link from "next/link";
-import { CartContent, CartPageContainer, CartWrapper } from "./page.styles";
+import {
+  CartContent,
+  CartPageContainer,
+  CartWrapper,
+  SelectQuantityButton,
+  SelectQuantityWrapper,
+} from "./page.styles";
 import { Box, Grid, Typography } from "@mui/material";
 
 export default function CartPage() {
@@ -21,68 +27,47 @@ export default function CartPage() {
         {items.map((item) => (
           <CartContent key={`${item.id}-${item.size}`}>
             {item.image && (
-              <Image
-                src={item.image}
-                alt={item.name}
-                width={80}
-                height={80}
-                style={{ borderRadius: 4 }}
-              />
+              <Link href={`/shop/${item.slug}`}>
+                <Image
+                  src={item.image}
+                  alt={item.name}
+                  width={80}
+                  height={80}
+                  style={{ borderRadius: 4 }}
+                />
+              </Link>
             )}
 
             <Box style={{ flex: 1 }}>
               <Link href={`/shop/${item.slug}`}>{item.name}</Link>
 
-              <div>{item.price} kr.</div>
+              <Typography>{item.price} kr.</Typography>
 
               {item.size && (
-                <div style={{ fontSize: 14, opacity: 0.7 }}>
+                <Typography style={{ fontSize: 14, opacity: 0.7 }}>
                   Størrelse: {item.size}
-                </div>
+                </Typography>
               )}
               {/* Update quantity */}
-              <div
-                style={{
-                  display: "flex",
-                  alignItems: "center",
-                  gap: 8,
-                  marginTop: 8,
-                }}
-              >
-                <button
+              <SelectQuantityWrapper>
+                <SelectQuantityButton
                   onClick={() =>
                     updateQty(item.id, item.quantity - 1, item.size)
                   }
-                  style={{
-                    width: 30,
-                    height: 30,
-                    borderRadius: 6,
-                    border: "1px solid #ccc",
-                    background: "white",
-                    cursor: "pointer",
-                  }}
                 >
                   -
-                </button>
+                </SelectQuantityButton>
 
-                <span>{item.quantity}</span>
+                <Box>{item.quantity}</Box>
 
-                <button
+                <SelectQuantityButton
                   onClick={() =>
                     updateQty(item.id, item.quantity + 1, item.size)
                   }
-                  style={{
-                    width: 30,
-                    height: 30,
-                    borderRadius: 6,
-                    border: "1px solid #ccc",
-                    background: "white",
-                    cursor: "pointer",
-                  }}
                 >
                   +
-                </button>
-              </div>
+                </SelectQuantityButton>
+              </SelectQuantityWrapper>
             </Box>
           </CartContent>
         ))}
